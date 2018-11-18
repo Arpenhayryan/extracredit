2b40c79026ad0baa3e2522589d0ae544a3a66187
Arpenhayryan <arpenik_hayryan@edu.aua.am>
2018-11-18 22:41:01 +0400

    Add files via upload

---
+++ java/src/extraCredit/Example.java
@@ -0,0 +1,11 @@
+package extraCredit;
+
+public class Example {
+	public static void main(String[] args){
+		Person p1 = new Person ();
+		Person p2 = new Person ();
+		Person p3 = new Person ();
+		System.out.println(p1.gender);
+	}
+
+}
---
+++ java/src/extraCredit/Person.java
@@ -0,0 +1,17 @@
+package extraCredit;
+
+public class Person {
+	public String name;
+	public int age;
+	public String gender; 
+	
+	public Person() {
+		
+		this.gender = "female";
+		
+	}
+	
+	public void cry() {
+		System.out.println("lol");
+	}
+}
